9f6c6da52d94395ee97e5cd1f8555297a0c6dfe0
Walmyr <wlsf82@gmail.com>
2026-01-07 23:40:12 +0100

    Update footer’s copyright

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
         <br></br>
       </main>
       <footer>
-      <p>Copyleft 😜 <a href="https://walmyr.dev" target="_blank" rel="noreferrer">Walmyr</a>, 2025</p>
+      <p>Copyleft 😜 <a href="https://walmyr.dev" target="_blank" rel="noreferrer">Walmyr</a>, 2026</p>
       </footer>
     </>
   );
